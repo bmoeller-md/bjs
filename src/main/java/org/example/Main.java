5ee40b97996c319c34da70073e20a0921ed59677
bmoeller-md <51704949+bmoeller-md@users.noreply.github.com>
2024-06-07 09:35:52 +0200

    -

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int i = 1;
+        if (i == 2) {
+            System.out.println("I = 2");
+        }
     }
 }
